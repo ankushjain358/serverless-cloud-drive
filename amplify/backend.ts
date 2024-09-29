2d693759a5cab5e3d0f3ae1ded81226e42a71ac5
Ankush Jain <ankushjain358@gmail.com>
2024-09-29 16:52:00 +0530

    Added sharp and ffmpeg layer (#2)
    Added sharp and ffmpeg layer

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,10 +3,16 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
 import { generateThumbnail } from './functions/generate-thmbnail/resource';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import 'dotenv/config'
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
+var sharpLambdaLayerArn = process.env.SHARP_LAMBDA_LAYER_ARN;
+var ffmpegLambdaLayerArn = process.env.FFMPEG_LAMBDA_LAYER_ARN;
+
+if (!sharpLambdaLayerArn || !ffmpegLambdaLayerArn) {
+  throw new Error('Missing required environment variables');
+}
+
 const backend = defineBackend({
   auth,
   data,
@@ -18,3 +24,8 @@ const backend = defineBackend({
 // Amplify Gen 2 enables guest access by default. To disable it, do the following changes:
 const { cfnIdentityPool } = backend.auth.resources.cfnResources;
 cfnIdentityPool.allowUnauthenticatedIdentities = false;
+
+// Add the Sharp Layer to the generateThumbnail function
+const generateThumbnailLambda = backend.generateThumbnail.resources.lambda as lambda.Function
+const sharpLambdaLayer = lambda.LayerVersion.fromLayerVersionArn(generateThumbnailLambda, "SharpLayer", sharpLambdaLayerArn)
+generateThumbnailLambda.addLayers(sharpLambdaLayer)
