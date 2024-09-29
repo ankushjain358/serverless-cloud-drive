d0c3833c7931f58dcf99e55f672d16de65e6c80c
Ankush Jain <ankushjain358@gmail.com>
2024-09-30 00:48:31 +0530

    Added eventbridge eventbus

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,6 +4,7 @@ import { data } from './data/resource';
 import { storage } from './storage/resource';
 import { generateThumbnail } from './functions/generate-thmbnail/resource';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as events from 'aws-cdk-lib/aws-events';
 import 'dotenv/config'
 
 var sharpLambdaLayerArn = process.env.SHARP_LAMBDA_LAYER_ARN;
@@ -21,11 +22,18 @@ const backend = defineBackend({
   generateThumbnail
 });
 
+const scope = backend.storage.resources.cfnResources.cfnBucket.stack;
+
 // Amplify Gen 2 enables guest access by default. To disable it, do the following changes:
 const { cfnIdentityPool } = backend.auth.resources.cfnResources;
 cfnIdentityPool.allowUnauthenticatedIdentities = false;
 
 // Add the Sharp Layer to the generateThumbnail function
 const generateThumbnailLambda = backend.generateThumbnail.resources.lambda as lambda.Function
-const sharpLambdaLayer = lambda.LayerVersion.fromLayerVersionArn(generateThumbnailLambda, "SharpLayer", sharpLambdaLayerArn)
+const sharpLambdaLayer = lambda.LayerVersion.fromLayerVersionArn(scope, "SharpLayer", sharpLambdaLayerArn)
 generateThumbnailLambda.addLayers(sharpLambdaLayer)
+
+ // Create a cloud-drive event bus
+ const cloudDriveEventBus = new events.EventBus(scope, 'CloudDriveEventBus', {
+  eventBusName: 'cloud-drive-event-bus'
+});
